fix memory leak from keeping gradients

mace/modules/llpr.py
@@ -30,8 +30,8 @@ def calibrate_llpr_params(
         batch_dict = batch.to_dict()
         y = batch_dict['energy']
         model_outputs = model(batch_dict)
-        predictions = model_outputs['energy']
-        ll_feats.append(model_outputs['ll_feats'])
+        predictions = model_outputs['energy'].detach()
+        ll_feats.append(model_outputs['ll_feats'].detach())
         actual_errors.append((y - predictions)**2)
 
     actual_errors = torch.cat(actual_errors, dim=0)

mace/modules/models.py
@@ -864,8 +864,8 @@ class LLPRScaleShiftMACE(torch.nn.Module):
             batch.to(self.covariance.device)
             batch_dict = batch.to_dict()
             output = self.forward(batch_dict)
-            ll_feats = output["ll_feats"]
-            self.covariance += ll_feats.T @ ll_feats
+            ll_feats = output["ll_feats"].detach()
+            self.covariance = self.covariance + ll_feats.T @ ll_feats
         self.covariance_computed = True
 
     def compute_inv_covariance(self, C: float, sigma: float) -> None: