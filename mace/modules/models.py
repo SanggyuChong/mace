e0b1aea9788033ee97a79aecd88be401947d5ccf
SanggyuChong <sanggyu.chong@epfl.ch>
2024-05-06 00:25:07 +0200

    bug fix for aggregation call

--- mace/modules/models.py
+++ mace/modules/models.py
@@ -1120,7 +1120,7 @@ class LLPRModel(torch.nn.Module):
         output = self.orig_model(
             data, (compute_force_uncertainty or compute_stress_uncertainty or compute_virial_uncertainty), compute_force, compute_virials, compute_stress, compute_displacement
         )
-        ll_feats = self.aggregate_features(output["node_feats"], data["batch"], num_graphs, num_atoms)
+        ll_feats = self.aggregate_features(output["node_feats"], data["batch"], num_graphs)
 
         energy_uncertainty = None
         force_uncertainty = None
